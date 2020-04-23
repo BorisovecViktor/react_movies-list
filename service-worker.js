289339c0dbd2ee25d6ee17ce25ca72f032d72caa
BorisovecViktor <viktorborisovets@gmail.com>
2020-04-23 15:59:14 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.5af9a0f6a3f45170f2de90e0fdf57e5a.js"
 );
 
 self.addEventListener('message', (event) => {
